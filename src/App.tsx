23b5d4e4aa6295a6f8a04e520a0ad90555511a8b
Gabriel <gaeidelman@gmail.com>
2022-12-05 11:35:52 -0800

    Updated Changes; I have to debug this version

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,49 @@
-// import React, { useState } from "react";
-// import "./App.css";
-// import Chessboard from "chessboardjsx";
-// import { ChessInstance, ShortMove } from "../node_modules/@types/chess.js";
-// import { Chess } from 'chess.js';
-
-
-// const App: React.FC = () => {
-//   const chess = new Chess("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKRNR w KQkq - 0 1");
-
-//   const [fen, setFen] = useState(chess.fen());
-
-//   const handleMove = (move: ShortMove) => {
-//     console.log("dropped")
-//     if (chess.move(move)) {
-//       setTimeout(() => {
-//         const moves = chess.moves();
-
-//         if (moves.length > 0) {
-//           const computerMove = moves[Math.floor(Math.random() * moves.length)];
-//           chess.move(computerMove);
-//           setFen(chess.fen());
-//         }
-//       }, 300);
-
-//       setFen(chess.fen());
-//     }
-//   };
-
-//   return (
-//     <div className="flex-center">
-//       <h1>Random Chess</h1>
-//       <Chessboard
-//         width={400}
-//         position="start"
-//         onDrop={(move) =>
-//           handleMove({
-//             from: move.sourceSquare,
-//             to: move.targetSquare,
-//             promotion: "q",
-//           })
-//         }
-//       />
-//     </div>
-//   );
-// };
-
-// export default App;
-
 import "./App.css";
-import Chessboard from "chessboardjsx";
+import React, { useState, useEffect } from "react";
+import Chessground from "@react-chess/chessground"
+import "chessground/assets/chessground.base.css";
+import "chessground/assets/chessground.brown.css";
+import "chessground/assets/chessground.cburnett.css";
+import { ShortMove } from "../node_modules/@types/chess.js";
+import { Chess } from 'chess.js';
+import { findConfigFile } from "typescript";
 
 const App: React.FC = () => {
 
+const[fren, setFren] = useState("rnbqk1nr/ppp1ppb1/6p1/3p3p/3P1P2/6PP/PPP1P1B1/RNBQK1NR b KQkq f3 0 5");
+const chess = new Chess(fren);
+const handleMove = (from: string, to: string) => {
+  // chess.move({from, to});
+  // setFren(chess.fen())
+  let newMove = chess.move({from, to});
+
+  if(newMove==null) {  
+    setFren(chess.fen());
+  }
+
+  else {
+    chess.move({from, to})
+}
+  };  
+
   return (
+    console.log(fren + "this is the fren that's playing"),
     <div className="flex-center">
-      <Chessboard
-      position = "start"
+      <Chessground
+        width={400}
+        height={400}
+        config = {{
+          fen: fren,
+          movable: {
+            
+          },
+          events: {
+            move: (orig, dest) => handleMove(
+            orig, dest)
+          }
+        }}
       />
+      
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,21 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import Chessground from "@react-chess/chessground"
+// import "chessground/assets/chessground.base.css";
+// import "chessground/assets/chessground.brown.css";
+// import "chessground/assets/chessground.cburnett.css";
+
+// const root = ReactDOM.createRoot(
+//   document.getElementById('root') as HTMLElement
+// );
+// root.render(
+//   <React.StrictMode>
+//     <Chessground />
+//   </React.StrictMode>
+// );
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
